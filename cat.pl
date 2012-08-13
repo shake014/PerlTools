51a8145c57744fc692407ab70aadbb3fbe749ccd
shake014 <shake014@umn.edu>
2012-08-13 16:12:24 -0500

    Added comments and shortened code.

--- cat.pl
+++ cat.pl
@@ -1,20 +1,18 @@
 #!/usr/bin/perl -w
+# Take in files as command line arguments and return contents
 use strict;
 use warnings;
-
-sub print_file {
-   my $files = shift;
-   open FILE, $files;
-   while (my $line = <FILE>){
-     print $line;
-   }
-   print "\n";
-}
-
+#Open named files and return contents line by line
 sub cat {
   while (my $files = shift){
-    print_file($files);
-  }   
+      open FILE, $files;
+          while (my $line = <FILE>){
+              print $line;
+          }
+          print "\n";
+  }
 }
 
+#Function call references command line arguments
 cat "@ARGV\n";
+exit;
