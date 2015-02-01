da75b05e780fddd7ac6970db90fac92985d10e53
shake014 <shake014@umn.edu>
2015-02-01 16:41:41 -0600

    Completed ls.pl

--- ls.pl
+++ ls.pl
@@ -1,1 +1,15 @@
 #!/usr/bin/perl -w
+
+use strict;
+use warnings;
+
+my $currentdirectory = cwd();
+
+opendir(DIR, $currentdirectory) or die $!;
+
+while(my $file = readdir(DIR)){
+    print "$file\n";
+}
+
+closedir(DIR);
+exit 1;
